960dd3accf2346ae7699c797245be7a71af74d8e
Jason Grlicky <jason@paracosm.us>
2017-11-20 09:28:54 -0800

    Added future API usage in the example

--- simulacrum/examples/simple.rs
+++ simulacrum/examples/simple.rs
@@ -5,6 +5,7 @@ use simulacrum::*;
 trait CoolTrait {
     fn foo(&self);
     fn bar(&mut self);
+    fn goop(&mut self, flag: bool) -> u32;
 }
 
 pub struct CoolTraitMock {
@@ -18,38 +19,62 @@ impl CoolTraitMock {
         }
     }
 
-    pub fn expect(&mut self, name: &'static str) -> TrackedMethod {
+    pub fn expect(&mut self, name: &'static str) -> Box<TrackedMethodT> {
         self.expectations.track_method(name)
     }
 
-    pub fn expect_foo(&mut self) -> TrackedMethod {
-        self.expect("foo")
+    pub fn expect_foo(&mut self) -> TrackedMethod<(), ()> {
+        self.expect("foo").downcast::<TrackedMethod<(), ()>>().unwrap()
     }
 
-    pub fn expect_bar(&mut self) -> TrackedMethod {
-        self.expect("bar")
+    pub fn expect_bar(&mut self) -> TrackedMethod<(), ()> {
+        self.expect("bar").downcast::<TrackedMethod<(), ()>>().unwrap()
+    }
+
+    pub fn expect_goop(&mut self) -> TrackedMethod<(bool), u32> {
+        self.expect("goop").downcast::<TrackedMethod<(bool), u32>>().unwrap()
     }
 }
 
 impl CoolTrait for CoolTraitMock {
     fn foo(&self) {
-        self.expectations.was_called("foo");
+        self.expectations
+            .was_called("foo")
+            .downcast::<TrackedMethod<(), ()>>()
+            .unwrap()
+            .with(())
+            .returning()
     }
 
     fn bar(&mut self) {
-        self.expectations.was_called("bar");
+        self.expectations
+            .was_called("bar")
+            .downcast::<TrackedMethod<(), ()>>()
+            .unwrap()
+            .with(())
+            .returning()
+    }
+
+    fn goop(&mut self, flag: bool) -> u32 {
+        self.expectations
+            .was_called("goop")
+            .downcast::<TrackedMethod<(bool), u32>>()
+            .unwrap()
+            .with((flag))
+            .returning()
     }
 }
 
 fn main() {
     // Set up expectations
     let mut m = CoolTraitMock::new();
-    m.expect_bar().called_once();
-    m.expect_foo().called_never();
+    m.expect_foo().called_once();
+    m.expect_bar().called_never();
+    m.expect_goop().called_once().with(true).returning(|| 5);
 
     // Execute test code
-    m.bar();
     m.foo();
+    assert_eq!(m.goop(true), 5);
 
     // When the ExpectationStore is dropped, the expectations will be evaluated
 }
