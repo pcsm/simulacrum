1a7f0e8e977c199f3ee0cbcdd5027fc779d911fb
Jason Grlicky <jason@paracosm.us>
2018-01-03 19:30:33 -0800

    Updated examples and Auto for modification behaviors

--- simulacrum_auto/examples/example.rs
+++ simulacrum_auto/examples/example.rs
@@ -40,7 +40,7 @@ fn main() {
     m.expect_boop().called_times(2);
     m.expect_store().called_once().with(deref(777));
     m.expect_toggle().called_once().with(deref(true))
-                                   .returning(|&arg| { unsafe { *arg.as_mut().unwrap() = false } });
+                                   .modifying(|&mut arg| { unsafe { *arg.as_mut().unwrap() = false } });
 
     // Execute test code
     m.foo();
